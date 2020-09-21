d87e1b7f1e22b3c81104e96ce85d0f7f8d18ce6d
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-21 15:37:01 +0200

    Front

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
+
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
 
 function App() {
   return (
-    <Provider store={store}>
+    <div className="App">
       <div className="App">
-        <h1>Hello wrld</h1>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </div>
-        </Router>
+        <h1>hello there</h1>
       </div>
-    </Provider>
+    </div>
   );
 }
 
