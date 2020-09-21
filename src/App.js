990c81b17f9290a7463cbeecf81bb488cd04b754
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-21 15:48:03 +0200

    Frint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/pages/home";
+import Header from "./components/layouts/header";
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
 
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
@@ -7,7 +11,14 @@ function App() {
   return (
     <div className="App">
       <div className="App">
-        <h1>hello there</h1>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </Router>
       </div>
     </div>
   );
