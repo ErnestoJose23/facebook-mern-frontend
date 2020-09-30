c5df72ef95c4d0868b1fd126315442807cfb3f87
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-30 12:03:12 +0200

    Style

--- src/components/layouts/FeedPost.js
+++ src/components/layouts/FeedPost.js
@@ -2,10 +2,14 @@ import { Avatar } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import Axios from "axios";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@material-ui/icons/ChatBubbleOutlineOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
 
 function FeedPost({ user_id, displayName, timestamp, title, imagename }) {
   const [user, setUser] = useState([]);
   const [img, setImg] = useState([]);
+  const [comment, setComment] = useState("");
   useEffect(() => {
     Axios.get(`http://localhost:5000/users/getUser/${user_id}`).then(
       (response) => {
@@ -41,6 +45,34 @@ function FeedPost({ user_id, displayName, timestamp, title, imagename }) {
       ) : (
         <div></div>
       )}
+      <div className="separador_post"></div>
+      <div className="post_actions">
+        <div className="post_option">
+          <ThumbUpAltOutlinedIcon />
+          Like
+        </div>
+        <div className="post_option">
+          <ChatBubbleOutlineOutlinedIcon />
+          Comment
+        </div>
+        <div className="post_option">
+          <ShareOutlinedIcon />
+          Share
+        </div>
+      </div>
+      <div className="separador_post"></div>
+      <div className="post_comment">
+        <Avatar src={user.avatar} />
+
+        <input
+          type="text"
+          className="messageSender_input"
+          placeholder="Write a comment..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          name="title"
+        />
+      </div>
     </div>
   );
 }
